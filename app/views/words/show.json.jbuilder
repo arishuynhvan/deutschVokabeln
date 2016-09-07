9d29fa0491ad53394e91d6d4229ac229be044d4b
arishuynhvan <arishuynhvan@gmail.com>
2016-09-07 15:32:46 +0000

    createad db

---
+++ app/views/words/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @word, :id, :deutsch, :remarks, :sample_sentence, :english, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907150608) do
+ActiveRecord::Schema.define(version: 20160907153159) do
 
   create_table "words", force: :cascade do |t|
     t.string   "deutsch"
+    t.text     "remarks"
     t.text     "sample_sentence"
     t.string   "english"
     t.datetime "created_at",      null: false
