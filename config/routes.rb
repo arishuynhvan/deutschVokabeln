f80ecd164d14b7b24e9c488ebb0d40b785700048
arishuynhvan <arishuynhvan@gmail.com>
2016-09-07 16:28:58 +0000

    Basic layout of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   resources :words
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "words#index"
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
